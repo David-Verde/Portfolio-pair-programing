c4ce1c7f7d6386d3b11d19393ff4880d2e0c22ce
Unyielding1 <99683363+Unyielding1@users.noreply.github.com>
2022-05-11 14:39:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -172,36 +172,4 @@ const displayPopupModal = (i) => {
 };
 document.querySelectorAll('.see-project').forEach((row) => row.addEventListener('click', () => displayPopupModal(row.getAttribute('data-index'))));
 
-/* End of display popup modal */
 
-/* Contact Form validation */
-// eslint-disable-next-line no-useless-escape
-const regex = '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$';
-document.querySelector('#form').addEventListener('submit', (e) => {
-  for (let i = 0; i < e.target.length; i += 1) {
-    if (e.target[i].classList.contains('form-control')) {
-      if (e.target[i].value === '') {
-        // e.target[i].nextElementSibling.style.display = 'block';
-        e.target[i].parentElement.nextElementSibling.innerText = `Please enter your ${e.target[i].getAttribute('placeholder')}`;
-        e.target[i].parentElement.nextElementSibling.style.color = 'red';
-        e.preventDefault();
-      } else if (e.target[i].type === 'input' && e.target[i].value.length > e.target[i].getAttribute('maxlength')) {
-        e.target[i].parentElement.nextElementSibling.innerText = `The maximum number of character is ${e.target[i].getAttribute('maxlength')}`;
-        e.target[i].parentElement.nextElementSibling.style.color = 'red';
-        e.preventDefault();
-      } else if (e.target[i].type === 'email' && e.target[i].value !== e.target[1].value.toLowerCase()) {
-        e.target[i].parentElement.nextElementSibling.innerText = 'Please enter your email in lowercase';
-        e.target[i].parentElement.nextElementSibling.style.color = 'red';
-        e.preventDefault();
-      } else if (e.target[i].type === 'email' && regex.test(e.target[i].value) !== e.target[i].value) {
-        e.target[i].parentElement.nextElementSibling.textContent = 'Please enter a valid email address';
-        e.target[i].parentElement.nextElementSibling.style.color = 'red';
-        e.preventDefault();
-      } else {
-        e.target[i].parentElement.nextElementSibling.style.display = 'none';
-        e.target[i].parentElement.nextElementSibling.innerText = '';
-      }
-    }
-  }
-});
-/* End of Contact Form Validation */
