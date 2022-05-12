d88eacfbb96e6969e0586cc9f47aa71e1936c166
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-12 15:14:20 -0500

    form storage fix

---
+++ form.js
@@ -0,0 +1,69 @@
+const form = document.querySelector('#form');
+const formName = document.querySelector('#name');
+const email = document.querySelector('#email');
+const formtext = document.querySelector('#text');
+const emailErrorMsg = document.querySelector('#email-error-msg');
+const inputData = {};
+const storage = window.localStorage;
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (
+      e.code === 22
+      || e.code === 1014
+      || e.name === 'QuotaExceededError'
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && (storage && storage.length !== 0);
+  }
+}
+
+function retrieveFormData() {
+  if (storageAvailable('localStorage')) {
+    const formDataString = storage.getItem('formData');
+    const formData = JSON.parse(formDataString);
+    return formData;
+  }
+  return false;
+}
+
+function populateForm() {
+  const formData = retrieveFormData();
+  if (formData) {
+    if (formData.name) {
+      formName.value = formData.name;
+    }
+    if (formData.email) {
+      email.value = formData.email;
+    }
+    if (formData.text) {
+      formtext.value = formData.text;
+    }
+  }
+}
+populateForm();
+
+formName.addEventListener('change', () => {
+  inputData.name = formName.value;
+  inputData.email = email.value;
+  inputData.text = formtext.value;
+  storage.setItem('formData', JSON.stringify(inputData));
+});
+
+email.addEventListener('change', () => {
+  inputData.name = formName.value;
+  inputData.email = email.value;
+  inputData.text = formtext.value;
+  storage.setItem('formData', JSON.stringify(inputData));
+});
+
+formtext.addEventListener('change', () => {
+  inputData.name = formName.value;
+  inputData.email = email.value;
+  inputData.text = formtext.value;
+  storage.setItem('formData', JSON.stringify(inputData));
+});
