9415be35222bf2749ccb3c575208472973f7f6da
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-12 20:04:30 -0500

    fix spaces lint

--- form.js
+++ form.js
@@ -1,14 +1,14 @@
 form.addEventListener('keyup', () => {
-    const formData = {
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      text: document.getElementById('text').value,
-    };
-    localStorage.setItem('formData', JSON.stringify(formData));
-  }
-  );
-    const formData = JSON.parse(localStorage.getItem('formData'));
-      document.getElementById('name').value = formData.name;
-      document.getElementById('email').value = formData.email;
-      document.getElementById('text').value = formData.text;
-  
+const formData = {
+name: document.getElementById('name').value,
+email: document.getElementById('email').value,
+text: document.getElementById('text').value,
+  };
+localStorage.setItem('formData', JSON.stringify(formData));
+},
+);
+const formData = JSON.parse(localStorage.getItem('formData'));
+document.getElementById('name').value = formData.name;
+document.getElementById('email').value = formData.email;
+document.getElementById('text').value = formData.text;
+
