5e5335fd11878693607e43e9bc50498a88171fc4
Unyielding1 <david.verde.alvarez@gmail.com>
2022-05-12 20:11:38 -0500

    Update form.js

--- form.js
+++ form.js
@@ -12,3 +12,12 @@ document.getElementById('name').value = formData.name;
 document.getElementById('email').value = formData.email;
 document.getElementById('text').value = formData.text;
 
+window.onload = () => {
+    let savedFormData = localStorage.getItem('contactFormInput');
+    savedFormData = JSON.parse(savedFormData);
+    // Check if the form data object is found on localStorage
+    if (savedFormData) {
+    // populate inputs values if data was found
+    // ex: nameInput.value = savedFormData.name
+    }
+  };
